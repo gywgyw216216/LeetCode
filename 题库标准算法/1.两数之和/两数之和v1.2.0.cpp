ed368b8e5939515d790be8be3df3560f211bfcc8
Yuwei Gu <56475647+gywgyw216216@users.noreply.github.com>
2021-09-08 07:43:10 +0800

    Create 两数之和v1.2.0.cpp

---
+++ 题库标准算法/1.两数之和/两数之和v1.2.0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result(2);
+        const int length = nums.size();
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            int temporality = nums.at(i);
+
+            for (int j = i + 1; j < length; j++)
+            {
+                if (temporality + nums.at(j) == target)
+                {
+                    result.at(0) = i;
+                    result.at(1) = j;
+
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+};
