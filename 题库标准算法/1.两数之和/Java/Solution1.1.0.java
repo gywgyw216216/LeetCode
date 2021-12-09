511bc8478d72e8c75591ea216689dc16f9a550df
Yuwei Gu <56475647+gywgyw216216@users.noreply.github.com>
2021-12-09 15:59:13 +0800

    v1.1.0

---
+++ 题库标准算法/1.两数之和/Java/Solution1.1.0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> number = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (!number.containsKey(nums[i]))
+            {
+                number.put(nums[i], i);
+            }
+
+            if (number.containsKey(target - nums[i]) && number.get(target - nums[i]) != i)
+            {
+                result[0] = number.get(target - nums[i]);
+                result[1] = i;
+
+                return result;
+            }
+        }
+
+        return result;
+    }
+}
