705316a9c8ce12e37b6c2c84d9c116b755deef84
Yuwei Gu <56475647+gywgyw216216@users.noreply.github.com>
2021-12-14 16:57:08 +0800

    v1.0.0

---
+++ 题库标准算法/2.两数相加/C++/Solution1.0.0.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode;
+        const ListNode* pl1 = l1, * pl2 = l2;
+        unsigned short carry = 0;
+
+        result = nullptr;
+
+        ListNode* r = result;
+
+        while(pl1 || pl2 || carry)
+        {
+            ListNode* p = new ListNode;
+            unsigned short usL1, usL2;
+            unsigned short value;
+
+            usL1 = usL2 = 10;
+
+            if(pl1)
+            {
+                usL1 = pl1->val;
+                pl1 = pl1->next;
+            }
+
+            if(pl2)
+            {
+                usL2 = pl2->val;
+                pl2 = pl2->next;
+            }
+
+            if(usL1 == 10 && usL2 < 10)
+                value = usL2 + carry;
+            else if(usL2 == 10 && usL1 < 10)
+                value = usL1 + carry;
+            else if(usL1 == 10 && usL2 == 10)
+                value = carry;
+            else
+                value = usL1 + usL2 + carry;
+
+            carry = value / 10;
+            value %= 10;
+            p->val = value;
+
+            if(result)
+                r->next = p;
+            else
+                result = p;
+
+            p->next = nullptr;
+            r = p;
+        }
+
+        return result;
+    }
+};
