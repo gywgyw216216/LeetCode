11b85dcb78ecedf9269837edecf64938e4338816
Yuwei Gu <1652423059@qq.com>
2022-01-13 11:11:10 +0800

    v1.0.0

---
+++ 题库标准算法/3.无重复字符的最长子串/C++/Solution1.0.0.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        const unsigned short length = (unsigned short)s.size();
+
+    if(length)
+    {
+        if(length == 1)
+            return 1;
+        else if(length == 2)
+        {
+            if(s[0] == s[1])
+                return 1;
+            else
+                return 2;
+        }
+        else
+        {
+            unsigned short result = 1;
+            unsigned short i = 0, j = 1;
+            string str = {s[i]};
+
+            while(j < length)
+            {
+                if(str.find(s[j]) == str.npos)
+                {
+                    str += s[j];
+                    j++;
+                }
+                else
+                {
+                    if(j - i > result)
+                        result = (unsigned short)(j - i);
+
+                    i++;
+                    str = s[i];
+                    j = (unsigned short)(i + 1);
+                }
+            }
+
+            if(j - i > result)
+                result = (unsigned short)(j - i);
+
+            return result;
+        }
+    }
+    else
+        return 0;
+    }
+};
