d5936a1cc872fc3085ca8ba131c93114ebe6a068
Yuwei Gu <1652423059@qq.com>
2022-01-14 16:42:55 +0800

    v1.0.0

--- 题库标准算法/3.无重复字符的最长子串/C++/Solution1.0.0.cpp
+++ 题库标准算法/3.无重复字符的最长子串/C++/Solution1.0.0.cpp
@@ -3,47 +3,43 @@ public:
     int lengthOfLongestSubstring(string s) {
         const unsigned short length = (unsigned short)s.size();
 
-    if(length)
-    {
-        if(length == 1)
-            return 1;
-        else if(length == 2)
+        if(length)
         {
-            if(s[0] == s[1])
+            if(length == 1)
                 return 1;
+            else if(length == 2)
+            {
+                if(s[0] == s[1])
+                    return 1;
+                else
+                    return 2;
+            }
             else
-                return 2;
+            {
+                unsigned short result = 1;
+                unsigned short i = 0, j = 1;
+                string str = {s[i]};
+
+                while(j < length)
+                {
+                    if(str.find(s[j]) == str.npos)
+                    {
+                        str += s[j];
+                        j++;
+                    }
+                    else
+                    {
+                        result = max((unsigned short)(j - i), result);
+                        str = s[++i];
+                        j = (unsigned short)(i + 1);
+                    }
+                }
+
+                result = max((unsigned short)(j - i), result);
+
+                return result;
+            }
         }
         else
-        {
-            unsigned short result = 1;
-            unsigned short i = 0, j = 1;
-            string str = {s[i]};
-
-            while(j < length)
-            {
-                if(str.find(s[j]) == str.npos)
-                {
-                    str += s[j];
-                    j++;
-                }
-                else
-                {
-                    if(j - i > result)
-                        result = (unsigned short)(j - i);
-
-                    str = s[++i];
-                    j = (unsigned short)(i + 1);
-                }
-            }
-
-            if(j - i > result)
-                result = (unsigned short)(j - i);
-
-            return result;
-        }
-    }
-    else
-        return 0;
-    }
+            return 0;
 };
